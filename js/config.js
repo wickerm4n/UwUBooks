f0e4dd0c4b7dd0b69702ffb7039cdf8e0967280b
wickerm4n <supp0rter@gmx.net>
2026-06-02 17:00:52 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
   window.UwUBooks = window.UwUBooks || {};
   window.UwUBooks.APP_CONFIG = Object.freeze({
     appName: 'UwU Books',
-    appVersion: '2026.06.02.1',
+    appVersion: '2026.06.02.4',
     storageKey: 'uwu-books.entries.v4',
     confirmationStorageKey: 'uwu-books.confirmations.v1',
     updateVersionUrl: 'version.json',
